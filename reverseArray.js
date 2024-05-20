9d6f0368b02655f62fb492c61341401d4f8e42a8
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-20 10:37:20 +0530

    Reverse Array if it's string

--- reverseArray.js
+++ reverseArray.js
@@ -1,3 +1,4 @@
+/*
 let arr=[1,2,3,4,5,6,7];
 for(start=0,end=arr.length-1;start<end;start++,end--){
     arr[start]=arr[start]+arr[end];//1+7=8;//arr[start]=8
@@ -5,3 +6,15 @@ for(start=0,end=arr.length-1;start<end;start++,end--){
     arr[start]=arr[start]-arr[end];//8-1=7//arr[start]=7
 }
 console.log(arr);//7, 6, 5, 4,3,2,1
+*/
+//Reverse array in place
+let arr = ["Apple","Orange","Grapes"];
+function reverse(arr){
+   for(let start=0,end=arr.length-1;start<end;start++,end--){
+     temp=arr[start];
+     arr[start]=arr[end];
+     arr[end]=temp;
+}
+return arr;
+}
+console.log(reverse(arr));
