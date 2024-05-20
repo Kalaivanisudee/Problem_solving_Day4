66d3298d8fbada17fc814d94fb3a5e7c0d79b0cd
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-20 11:21:02 +0530

    Remove Duplicate string  in an array

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,14 +1,14 @@
-let arr=[1,2,3,2,5,3];
+let arr=["apple","grapes","apple","orange","grapes"];
 let obj={}
 for(let i=0;i<arr.length;i++){
     obj[arr[i]]=true;
 
 }
-console.log(obj);//{ '1': true, '2': true, '3': true, '5': true }
+console.log(obj);//{ apple: true, grapes: true, orange: true }
 arr.length=0;
 console.log(arr);//[]
 for(let key in obj){
-    arr[arr.length]=+key;
+    arr[arr.length]=key;
 }
-console.log(arr);//[ 1, 2, 3, 5 ]
+console.log(arr);//[ 'apple', 'grapes', 'orange' ]
 
