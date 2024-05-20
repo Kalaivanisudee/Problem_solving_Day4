e1cb0904ac18ce4f439dd3ae59c3b5268b69e64b
kalaivanisudee <gkalaisakthi22@gmail.com>
2024-05-20 10:25:14 +0530

    Reverse Array

---
+++ reverseArray.js
@@ -0,0 +1,7 @@
+let arr=[1,2,3,4,5,6,7];
+for(start=0,end=arr.length-1;start<end;start++,end--){
+    arr[start]=arr[start]+arr[end];//1+7=8;//arr[start]=8
+    arr[end]=arr[start]-arr[end];//8-7=1;//arr[end]=1
+    arr[start]=arr[start]-arr[end];//8-1=7//arr[start]=7
+}
+console.log(arr);//7, 6, 5, 4,3,2,1
